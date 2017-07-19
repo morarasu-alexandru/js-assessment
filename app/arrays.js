8b5804d00b767ca73f2d01e4df5eb6f399fe740f
alexandru-morarasu <morarasu_alexandru@yahoo.com>
2017-07-19 18:10:29 +0300

    dev

--- app/arrays.js
+++ app/arrays.js
@@ -1,59 +1,87 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.arraysAnswers = {
-  indexOf: function(arr, item) {
+	indexOf: function (arr, item) {
+		return arr.indexOf(item);
+	},
 
-  },
+	sum: function (arr) {
+		var sum = 0;
+		arr.forEach(function (elem) {
+			sum = sum + elem;
+		});
+		return sum;
+	},
 
-  sum: function(arr) {
+	remove: function (arr, item) {
 
-  },
+		for (var i = arr.length - 1; i >= 0; i--) {
+			if (arr[i] === item) {
+				arr.splice(i, 1);
+			}
+		}
+		return arr;
 
-  remove: function(arr, item) {
+	},
 
-  },
+	removeWithoutCopy: function (arr, item) {
+		for (var i = arr.length - 1; i >= 0; i--) {
+			if (arr[i] === item) {
+				arr.splice(i, 1);
+			}
+		}
+		return arr;
 
-  removeWithoutCopy: function(arr, item) {
+	},
 
-  },
+	append: function (arr, item) {
 
-  append: function(arr, item) {
+		arr.push(item);
+		return arr;
+	},
 
-  },
+	truncate: function (arr) {
+	var last = _.last(arr);
+	arr.pop(last);
 
-  truncate: function(arr) {
+	return arr;
+	},
 
-  },
+	prepend: function (arr, item) {
+		arr.unshift(item);
+		return arr;
 
-  prepend: function(arr, item) {
+	},
 
-  },
+	curtail: function (arr) {
+		var first = _.first(arr);
+		arr.shift(first);
 
-  curtail: function(arr) {
+		return arr;
 
-  },
+	},
 
-  concat: function(arr1, arr2) {
+	concat: function (arr1, arr2) {
+		return concat(arr1, arr2);
+	},
 
-  },
+	insert: function (arr, item, index) {
 
-  insert: function(arr, item, index) {
+	},
 
-  },
+	count: function (arr, item) {
 
-  count: function(arr, item) {
+	},
 
-  },
+	duplicates: function (arr) {
 
-  duplicates: function(arr) {
+	},
 
-  },
+	square: function (arr) {
 
-  square: function(arr) {
+	},
 
-  },
+	findAllOccurrences: function (arr, target) {
 
-  findAllOccurrences: function(arr, target) {
-
-  }
+	}
 };
